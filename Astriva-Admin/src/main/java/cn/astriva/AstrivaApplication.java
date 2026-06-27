ad10eabdf1b29ae85a81ad2c0a780bb52f037484
Mr. Tao <1300930042@qq.com>
2026-06-27 14:21:49 +0800

    搭建基础框架

---
+++ Astriva-Admin/src/main/java/cn/astriva/AstrivaApplication.java
@@ -0,0 +1,19 @@
+package cn.astriva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 星驰快速开发模板启动类
+ *
+ * @author Mr. Tao
+ */
+@SpringBootApplication
+public class AstrivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrivaApplication.class, args);
+		System.out.println("〘星驰〙启动成功！");
+	}
+
+}
